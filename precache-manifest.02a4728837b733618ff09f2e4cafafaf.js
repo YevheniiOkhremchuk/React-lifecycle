071185bb9bb8486fdf7b7b6bd7d2e484cf82e6a5
Yevhenii <okhremchukjevhenii@.gmail.com>
2020-09-14 01:31:30 +0300

    Updates

---
+++ precache-manifest.02a4728837b733618ff09f2e4cafafaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c40e528b0fe6aed68cbcf095a6bb4d",
+    "url": "/React-lifecycle.git/index.html"
+  },
+  {
+    "revision": "a123d5a4b6a0ffd7d285",
+    "url": "/React-lifecycle.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a24d8cc04cceee49a285",
+    "url": "/React-lifecycle.git/static/js/2.8a2690ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-lifecycle.git/static/js/2.8a2690ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a123d5a4b6a0ffd7d285",
+    "url": "/React-lifecycle.git/static/js/main.d8412cf1.chunk.js"
+  },
+  {
+    "revision": "03de3bdae6810ed667c9",
+    "url": "/React-lifecycle.git/static/js/runtime-main.dc8b2207.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-lifecycle.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
