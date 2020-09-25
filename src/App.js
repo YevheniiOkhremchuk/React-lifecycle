6f0398d2372fc890cb1d9cc2b9f21c82dbb06381
Yevhenii <okhremchukjevhenii@.gmail.com>
2020-09-25 09:30:45 +0300

    HW-17

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Timer from './components/Timer';
 import './index.css';
-import './App.css';
 
 function App() {
   return (
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,50 +2,42 @@ import React, { useState, useEffect } from "react";
 
 const Timer = () => {
     const [time, setTime] = useState(30);
-    const [isActive, setIsActive] = useState(true);
+    const [isActive, setIsActive] = useState(false);
 
-   /* function toggle() {
-       setIsActive(!isActive);
-     } */
-      function reset() {
+    function switching() {
+        setIsActive(!isActive);
+    }
+    function reset() {
         setTime(30);
         setIsActive(false);
-      }
-       
-        useEffect(() => {
-        let interval = null;
+    }
+    useEffect(() => {
         console.log(`Залишилось часу: ${time}`);
-        
+        let interval; 
         if (isActive) {
             interval = setInterval(() => {
-            setTime((time) => (time > 0 ? time -1 : reset()));
-        }, 1000);
-        } else if (isActive && time !== 0) {
-        clearInterval(interval);
+                if (time > 0) {
+                    setTime(time -1 )
+                } else {
+                    reset()
+                }
+            }, 1000);
+        } else  {
+            clearInterval(interval);
         }          
         return () => clearInterval(interval);
         }, [isActive, time]);      
 
     return (
-        <div className='display'>
+        <div>
             <div>{time}</div>
             <div>
-            <button
-                onClick={() => {
-                    setIsActive(!isActive);
-                    //timerChange();
-                }}
-            >
-                {isActive ? 'Stop' : 'Start'}
-            </button>
-        </div>
-   
-        <div
-           className='display-row'
-           style={{ width: `calc(${time} * (100% / ${useState(30)}))`}}
-        ></div>
+                <button onClick={switching}>{isActive ? 'Pause' : 'Start'}</button>
+                <button onClick={reset}>Reset</button>
             </div>
-            );
-        };
+            <div style={{width: `calc(${time} * (100% / ${useState(30)}))`, height: "20px", backgroundColor: "#10101145"}}></div>
+        </div>
+    );
+};
         
 export default Timer;
